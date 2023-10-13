58da892afc6d6330f1f5e5c71c3d6ff0260507cd
Георги Сталев <sttallev99@gmail.com>
2023-10-13 16:19:47 +0300

    create pages and install and setup react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes, Route, Navigate} from 'react-router-dom';
+
+import Chat from './pages/Chat';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<Chat />}/>
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='*' element={<Navigate to='/' />} />
+      </Routes>
     </div>
   );
 }
