07a449dccfac1cfa2d5437267a75505321877d65
Георги Сталев <sttallev99@gmail.com>
2023-10-14 13:42:43 +0300

    Customize Login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Alert, Button, Form, Row, Col, Stack } from 'react-bootstrap';
 
 export default function Login() {
   return (
     <div>
-      Login page
-    </div>
+    <Form>
+      <Row style={{height: '100ch', justifyContent: 'center', paddingTop: '10%'}}>
+        <Col xs={6}>
+        <Stack gap={3}>
+          <h2>Login</h2>
+          <Form.Control type='text'  placeholder='Email'/>
+          <Form.Control type='text'  placeholder='Password'/>
+          <Button variant='primary' type='submit'>Login</Button>
+          <Alert variant='danger'><p>An error occured</p></Alert>
+          </Stack>
+        </Col>
+      </Row>
+    </Form>
+  </div>
   )
 }
