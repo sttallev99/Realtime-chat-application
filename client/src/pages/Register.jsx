3c8408ce8d1a81c242db45e6568d149d888a1f72
Георги Сталев <sttallev99@gmail.com>
2023-10-14 14:36:18 +0300

    Set up context

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,9 @@
 import { Alert, Button, Form, Row, Col, Stack } from 'react-bootstrap';
+import { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
 
 export default function Register() {
+  const { registerInfo, updateRegisterInfo } = useContext(AuthContext);  
   return (
     <div>
       <Form>
@@ -8,9 +11,9 @@ export default function Register() {
           <Col xs={6}>
           <Stack gap={3}>
             <h2>Register</h2>
-            <Form.Control type='text'  placeholder='Name'/>
-            <Form.Control type='text'  placeholder='Email'/>
-            <Form.Control type='text'  placeholder='Password'/>
+            <Form.Control type='text'  placeholder='Name' onChange={(e) => updateRegisterInfo({...registerInfo, name: e.target.value})}/>
+            <Form.Control type='text'  placeholder='Email' onChange={(e) => updateRegisterInfo({...registerInfo, email: e.target.value})}/>
+            <Form.Control type='text'  placeholder='Password' onChange={(e) => updateRegisterInfo({...registerInfo, password: e.target.value})}/>
             <Button variant='primary' type='submit'>Register</Button>
             <Alert variant='danger'><p>An error occured</p></Alert>
             </Stack>
