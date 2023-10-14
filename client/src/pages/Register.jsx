b73570a76b3bb86dfc76c69bf475f7ba4fda7e4b
Георги Сталев <sttallev99@gmail.com>
2023-10-14 13:41:04 +0300

    Customize Register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { Alert, Button, Form, Row, Col, Stack } from 'react-bootstrap';
 
 export default function Register() {
   return (
     <div>
-      Register page
+      <Form>
+        <Row style={{height: '100ch', justifyContent: 'center', paddingTop: '10%'}}>
+          <Col xs={6}>
+          <Stack gap={3}>
+            <h2>Register</h2>
+            <Form.Control type='text'  placeholder='Name'/>
+            <Form.Control type='text'  placeholder='Email'/>
+            <Form.Control type='text'  placeholder='Password'/>
+            <Button variant='primary' type='submit'>Register</Button>
+            <Alert variant='danger'><p>An error occured</p></Alert>
+            </Stack>
+          </Col>
+        </Row>
+      </Form>
     </div>
   )
 }
