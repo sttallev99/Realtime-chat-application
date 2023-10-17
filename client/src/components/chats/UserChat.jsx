d0eef95a88b30d41cc73ccb69b7dcc65ffd74561
Георги Сталев <sttallev99@gmail.com>
2023-10-17 19:48:27 +0300

    List Chats

---
+++ client/src/components/chats/UserChat.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Stack } from 'react-bootstrap';
+
+import { useFetchRecipient } from '../../hooks/useFetchRecipient';
+import avatar from '../../assets/avatar.svg'
+
+export default function UserChat(chat, user) {
+    const {recipientUser} = useFetchRecipient(chat, user);
+
+  return (
+    <Stack direction='horizontal' gap={3} className='user-card align-items-center p-2 justify-content-between' role='button'>
+        <div className="d-flex">
+            <div className="me-2">
+                <img src={avatar} height='35px' />
+            </div>
+            <div className="text-content">
+                <div className="name">{recipientUser.name}</div>
+                <div className="text">Text Message</div>
+            </div>
+        </div>
+        <div className="d-flex flex-column align-items-end">
+            <div className="date">12/12/2023</div>
+            <div className="this-user-notifications">2</div>
+            <span className='user-online'></span>
+        </div>
+    </Stack>
+  )
+}
---
+++ client/src/hooks/useFetchRecipient.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+import { baseURL, getRequest } from "../utils/services";
+
+export const useFetchRecipient = ({chat, user}) => {
+    const [error, setError] = useState(null);
+    const [recipientUser, setRecipientUser] = useState(null);
+
+    const recipientId = chat?.members.find((id) => id !== user._id);
+
+    useEffect(() => {
+        const getUser = async() => {
+            if(!recipientId) return null;
+
+            const response = await getRequest(`${baseURL}/users/find/${recipientId}`);
+
+            if(response.error) {
+                return setError(error);
+            }
+
+            setRecipientUser(response);
+        }
+
+        getUser();
+    }, []);
+
+    return { recipientUser }
+};
